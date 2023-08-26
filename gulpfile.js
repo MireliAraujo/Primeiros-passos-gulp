debb5f57b4cfa4c2687fa1f911f6112c4babc7f0
Mireli Araujo <112290519+jonascipriano48@users.noreply.github.com>
2023-08-26 19:13:34 -0300

    finalização do módulo de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+
+function comprimeImages() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
 
 function compilaSass(){
     return gulp.src('./source/styles/main.scss')
@@ -13,24 +30,8 @@ function compilaSass(){
         
 }
 
-function funcaoPadrao(callback) {
-    console.log("Executando via gulp");
-    callback();
-}
-
-function dizOi(callback) {
-    console.log("Olá gulp");
-    diizTchau();
-    callback();
-}
-
-function diizTchau() {
-    console.log("tchau gulp")
-}
-
-exports.default = gulp.series(funcaoPadrao, dizOi);
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
-exports.watch = function() {
+exports.default = function() {
     gulp.watch('./source/styles/*.scss', { ignoreInitial:false }, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial:false }, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', { ignoreInitial:false }, gulp.series(comprimeImages));
 }
